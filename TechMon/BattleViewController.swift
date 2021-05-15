e37e6e47776fa56aadab50e01706065c14919ded
TomohiroKawakami <kyuprgnbrz@gmail.com>
2021-05-15 12:35:42 +0900

    lobbyViewControllerまで

---
+++ TechMon/BattleViewController.swift
@@ -0,0 +1,50 @@
+//
+//  BattleViewController.swift
+//  TechMon
+//
+//  Created by 川上知宏 on 2021/05/15.
+//
+
+import UIKit
+
+class BattleViewController: UIViewController {
+    
+    @IBOutlet var playerNameLabel: UILabel!
+    @IBOutlet var playerImageView: UIImageView!
+    @IBOutlet var playerHPLable: UILabel!
+    @IBOutlet var playerMPLabel: UILabel!
+    
+    @IBOutlet var enemyNameLabel: UILabel!
+    @IBOutlet var enemyImageView: UIImageView!
+    @IBOutlet var enemyHPLabel: UILabel!
+    @IBOutlet var enemyMPLabel: UILabel!
+    
+    let techMonManager = TechMonManager.shared
+    
+    var playerHP = 100
+    var playerMP = 0
+    var enemyHP = 200
+    var enemyMP = 0
+    
+    var gameTimer: Timer!
+    var isPlayerAttackAvailable: Bool = true
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
